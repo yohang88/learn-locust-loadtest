43525185da36ee39fd1cc69e1cd058558b61befc
Yoga Hanggara <yohang88@gmail.com>
2020-07-22 23:42:09 +0700

    fast http
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- locustfile.py
+++ locustfile.py
@@ -1,15 +1,16 @@
-from locust import HttpUser, between, task, TaskSet
+from locust import between, task, TaskSet
+from locust.contrib.fasthttp import FastHttpUser
 
 
 class PublicBeneficiariesSection(TaskSet):
     @task
     def public_list_beneficiaries(self):
-        self.client.get(url="v1/pub/beneficiaries", headers={"accept": "application/json"})
+        self.client.get("v1/pub/beneficiaries", headers={"accept": "application/json"})
         pass
 
     @task
     def public_list_beneficiaries_bnba(self):
-        self.client.get(url="v1/pub/beneficiaries-bnba", headers={"accept": "application/json"})
+        self.client.get("v1/pub/beneficiaries-bnba", headers={"accept": "application/json"})
         pass
 
     @task
@@ -17,11 +18,11 @@ class PublicBeneficiariesSection(TaskSet):
         self.interrupt()
 
 
-class ApiUser(HttpUser):
+class ApiUser(FastHttpUser):
     wait_time = between(5, 15)
     tasks = {PublicBeneficiariesSection: 2}
 
-    @task
-    def ping(self):
-        self.client.get(url="ping", headers={"accept": "application/json"})
-        pass
+    # @task
+    # def ping(self):
+    #     self.client.get("ping", headers={"accept": "application/json"})
+    #     pass
