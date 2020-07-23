13ab935c0156728371ef0cb17fd0923514a0b81d
Yoga Hanggara <yohang88@gmail.com>
2020-07-23 09:05:19 +0700

    authenticate, login
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,30 @@
 from locust import between, task, TaskSet
 from locust.contrib.fasthttp import FastHttpUser
+import json
 
+class AuthenticateAdminSection(TaskSet):
+    auth_token = None
+
+    def login(self):
+        data = {"LoginForm": {"username": "staffkotabandung", "password": "123456"}}
+        response = self.client.post("v1/staff/login", data=json.dumps(data), headers={"accept": "application/json","content-type": "application/json"})
+        if response:
+            response_json = response.json()
+            self.auth_token = response_json['data']['access_token']
+        pass
+
+    def on_start(self):
+        self.login()
+
+    @task
+    def get_profile(self):
+        if self.auth_token:
+            self.client.get("v1/staff/me", headers={"accept": "application/json", "authorization": f"Bearer {self.auth_token}"})
+        pass
+
+    @task
+    def stop(self):
+        self.interrupt()
 
 class PublicBeneficiariesSection(TaskSet):
     @task
@@ -20,7 +44,7 @@ class PublicBeneficiariesSection(TaskSet):
 
 class ApiUser(FastHttpUser):
     wait_time = between(5, 15)
-    tasks = {PublicBeneficiariesSection: 2}
+    tasks = {AuthenticateAdminSection: 10, PublicBeneficiariesSection: 2}
 
     # @task
     # def ping(self):
